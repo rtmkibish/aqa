2a7b9a101a1fe4a8cff057920dc0c1518a4d06de
artem kibish <rtmkibish@gmail.com>
2021-09-23 19:58:27 +0300

    Lesson 8 Classwork

---
+++ src/main/java/hillel/lesson8/Classwork.java
@@ -0,0 +1,9 @@
+package hillel.lesson8;
+
+public class Classwork {
+    public static void main(String[] args) {
+        String str1 = "hello";
+        String str2 = "hello";
+        System.out.println(str1.compareToIgnoreCase(str2));
+    }
+}
